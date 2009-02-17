668f6e04f51b58db428de422eae526ec6a70bc5c
Simon Jefford <simon.jefford@gmail.com>
2009-02-17 17:33:07 +0000

    Make rspec optional

--- main.rb
+++ main.rb
@@ -21,11 +21,13 @@ run "cp config/database.yml config/example_database.yml"
 
 git :add => ".", :commit => "-a -m 'Initial Commit'"
 
-plugin 'rspec', :git => 'git://github.com/dchelimsky/rspec.git'
-plugin 'rspec-rails', :git => 'git://github.com/dchelimsky/rspec-rails.git'
-generate("rspec")
+if yes?("Use rspec?")
+  plugin 'rspec', :git => 'git://github.com/dchelimsky/rspec.git'
+  plugin 'rspec-rails', :git => 'git://github.com/dchelimsky/rspec-rails.git'
+  generate("rspec")
 
-git :add => ".", :commit => "-a -m 'Added rspec'"
+  git :add => ".", :commit => "-a -m 'Added rspec'"
+end
 
 generate("cucumber")
 
