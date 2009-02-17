8c0a12888bffcdd1f13b0cd058a42cd669105864
Simon Jefford <simon.jefford@gmail.com>
2009-02-17 14:48:43 +0000

    Initial

---
+++ main.rb
@@ -0,0 +1,32 @@
+inside('vendor') do
+  run "ln -s ~/code/rails/rails_trunk rails"
+end
+
+git :init
+
+file '.gitignore', <<-CODE
+log/*.log
+log/*.pid
+db/*.db
+db/*.sqlite3
+db/schema.rb
+tmp/**/*
+.DS_Store
+doc/api
+doc/app
+config/database.yml
+CODE
+
+run "cp config/database.yml config/example_database.yml"
+
+git :add => ".", :commit => "-a -m 'Initial Commit'"
+
+plugin 'rspec', :git => 'git://github.com/dchelimsky/rspec.git'
+plugin 'rspec-rails', :git => 'git://github.com/dchelimsky/rspec-rails.git'
+generate("rspec")
+
+git :add => ".", :commit => "-a -m 'Added rspec'"
+
+generate("cucumber")
+
+git :add => ".", :commit => "-a -m 'Added cucumber'"
